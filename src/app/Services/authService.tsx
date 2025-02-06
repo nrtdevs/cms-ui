c9aeea7588359e5b42dc322c284d611637e49f68
sailnrt <159438562+sailnrt@users.noreply.github.com>
2025-02-06 17:14:38 +0530

    /callback/credentials

---
+++ src/app/Services/authService.tsx
@@ -0,0 +1,49 @@
+import axios from 'axios'
+
+interface LoginResponse {
+  accessToken: string
+  user: {
+    id: number
+    email: string
+    name: string
+  }
+}
+
+export const login = async (email: string, password: string): Promise<LoginResponse> => {
+  const apiUrl = process.env.NEXT_PUBLIC_API_URL
+
+  if (!apiUrl) {
+    throw new Error('API URL is not defined in environment variables.')
+  }
+
+  const headers: {
+    'Content-Type': string
+    'Cache-Control': string
+    Pragma: string
+    Expires: string
+    Authorization?: string
+  } = {
+    'Content-Type': 'application/json',
+    'Cache-Control': 'no-cache, no-store, must-revalidate, post-check=0, pre-check=0',
+    Pragma: 'no-cache',
+    Expires: '0'
+  }
+
+  const accessToken = sessionStorage.getItem('accessToken')
+
+  if (accessToken) {
+    headers['Authorization'] = accessToken
+  }
+
+  try {
+    const response = await axios.post(`${apiUrl}/auth/login`, { email, password }, { headers })
+
+    console.log('Login Success:', response.data)
+
+    return response.data
+  } catch (error: any) {
+    console.error('Login Error:', error.response?.data?.message || error.message || error)
+
+    throw new Error(error.response?.data?.message || 'An unexpected error occurred during login')
+  }
+}
